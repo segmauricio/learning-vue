4690226da3abf84b6eac6a0be242037791f2e632
segmauricio <80988655+segmauricio@users.noreply.github.com>
2023-06-05 09:57:11 -0400

    Indecision App

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,16 +1,21 @@
 <template>
+  <!--
   <img alt="Vue logo" src="./assets/logo.png">
   <Counter title="Entraron" :start="3"/>
   <Counter :start="1"/>
+  -->
+  <Indecision/>
 </template>
 
 <script>
 import Counter from './components/Counter.vue'
+import Indecision from './components/Indecision.vue'
 
 export default {
   name: 'App',
   components: {
-    Counter
+    Counter,
+    Indecision
   }
 }
 </script>
---
+++ 03-fundamentos/src/components/Indecision.vue
@@ -0,0 +1,98 @@
+<template>
+    <h1>Indecision App</h1>
+    <img v-if=img :src="img" alt="background-image" />
+    <div class="bg-dark"></div>
+    <div class="indecision-container">
+        <input type="text" placeholder="Hazme una pregunta" v-model="question" />
+        <p>Recuerda terminar con un signo de interrogacion (?)</p>
+        <div v-if="isValidQuestion">
+            <h2>{{ question }}</h2>
+            <h1>{{ answer === 'yes' ? 'Si' : 'Naur'}}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import { isValid } from 'ipaddr.js';
+
+
+export default {
+    data() {
+        return {
+            question: null,
+            answer: null,
+            img: null,
+            isValidQuestion: false
+        }
+    },
+    methods: {
+        async getAnswer() {
+            this.answer = "Pensando..."
+            const { answer, image } = await fetch('https://yesno.wtf/api').then(res => res.json())
+            console.log(answer);
+            this.answer = answer;
+            //this.answer = answer === 'yes' ? 'Si!' : 'No!';
+            this.img = image;
+
+        }
+    },
+    watch: {
+        question(value, oldValue) {
+            //console.log({value, oldValue});
+            //console.log(value.includes('?'));
+            if (!value.includes('?')) return
+                this.isValidQuestion = true
+            //TODO: Realizar peticion http
+            this.getAnswer()
+        }
+    }
+};
+</script>
+
+<style scoped>
+img,
+.bg-dark {
+    height: 100vh;
+    left: 0px;
+    max-height: 100%;
+    max-width: 100%;
+    position: fixed;
+    top: 0px;
+    width: 100vw;
+}
+
+.bg-dark {
+    background-color: rgba(0, 0, 0, 0.4);
+}
+
+.indecision-container {
+    position: relative;
+    z-index: 99;
+}
+
+input {
+    width: 250px;
+    padding: 10px 15px;
+    border-radius: 5px;
+    border: none;
+}
+
+input:focus {
+    outline: none;
+}
+
+p {
+    color: white;
+    font-size: 20px;
+    margin-top: 0px;
+}
+
+h1,
+h2 {
+    color: white;
+}
+
+h2 {
+    margin-top: 150px;
+}
+</style>
