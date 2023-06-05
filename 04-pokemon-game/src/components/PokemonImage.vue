baf5aaf1791bc16de04b3e34edbfd30485c917fe
segmauricio <80988655+segmauricio@users.noreply.github.com>
2023-06-05 16:52:00 -0400

    Fixed image issue

--- 04-pokemon-game/src/components/PokemonImage.vue
+++ 04-pokemon-game/src/components/PokemonImage.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="pokemon-container">
         <img
+            v-if="!showPokemon"
             :src="imgSrc"
             class="hidden-pokemon"
             alt="pokemon-image" />
         <img 
-            v-if="showPokemon"
+            v-else
             :src="imgSrc"
             class="fade-in"
             alt="pokemon-image" />
@@ -40,8 +41,6 @@ export default {
 
 img {
     height: 200px;
-    position: absolute;
-    right: 32%;
     user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
