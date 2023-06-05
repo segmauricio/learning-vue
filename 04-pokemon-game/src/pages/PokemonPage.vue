4534c4ee2238ac963e9be9754f7806396d4886ac
segmauricio <80988655+segmauricio@users.noreply.github.com>
2023-06-05 16:38:56 -0400

    Finished the logic behind the game

--- 04-pokemon-game/src/pages/PokemonPage.vue
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -3,8 +3,16 @@
     <h1 v-if="!pokemon">Wait for it...</h1>
     <div v-else>
       <h1>Who is this Pokemon?</h1>
+
       <PokemonImage :pokemonId="pokemon.id" :showPokemon="showPokemon" />
-      <PokemonOptions :pokemons="pokemonArr" />
+      <PokemonOptions :pokemons="pokemonArr" @selectionPokemon="checkAnswer" />
+
+      <template v-if="showAnswer">
+        <h2 class="fade-in">
+          {{ message }}
+        </h2>
+        <button @click="newGame" v-if="showPokemon">New Game</button>
+      </template>
     </div>
   </div>
 </template>
@@ -26,15 +34,34 @@ export default {
     return {
       pokemonArr: [],
       pokemon: null,
-      showPokemon: false
+      showPokemon: false,
+      showAnswer: false,
+      message: ''
     }
   },
   methods: {
     async mixPokemonArray() {
       this.pokemonArr = await getPokemonOptions()
-
       const randomInt = Math.floor(Math.random() * 4)
       this.pokemon = this.pokemonArr[randomInt]
+    },
+    checkAnswer(selectedId) {
+     
+      this.showAnswer = true
+
+      if (selectedId === this.pokemon.id) {
+        this.message = `You guessed ${this.pokemon.name} right!`
+        this.showPokemon = true
+      } else {
+        this.message = 'Try again.'
+      }
+    },
+    newGame(){
+      this.showAnswer = false,
+      this.showPokemon = false,
+      this.pokemonArr = [],
+      this.pokemon = null,
+      this.mixPokemonArray()
     }
   },
   mounted() {
