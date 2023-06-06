8f8bd25340069caa57708c021a89f34c48f04250
segmauricio <80988655+segmauricio@users.noreply.github.com>
2023-06-06 17:22:27 -0400

    Fixed comment

--- 05-rutas-ciclo/src/router/router.js
+++ 05-rutas-ciclo/src/router/router.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
+// 2. Define some routes Each route should map to a component. We'll talk about nested routes later.
 const routes = [
     { 
         path: "/",
@@ -29,9 +27,7 @@ const routes = [
     },
 ];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
+// 3. Create the router instance and pass the `routes` option. You can pass in additional options here, but let's keep it simple for now.
 const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHashHistory(),
